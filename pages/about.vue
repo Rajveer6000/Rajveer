41b3cb36ac8b87aadbdfee633d9e480a287b3029
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2023-06-18 20:58:54 +0530

    Create about.vue

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <Navbar/>
+  <p>  hello rajveer </p>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from '~/components/navbar.vue';
+
+export default Vue.extend({
+    name: "about",
+    components: { Navbar }
+})
+</script>
+
+
+<style >
+template {
+  font-family: 'Helvetica Now', sans-serif;
+}
+</style>
